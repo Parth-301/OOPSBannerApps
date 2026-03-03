a1f8fa50c569830a97bd315328c3c8c512ce3deb
PARTH GUPTA <pg0499@srmist.edu.in>
2026-03-03 10:41:09 +0530

    we used arrays and loops

---
+++ UC4ArraysAndLoops.java
@@ -0,0 +1,18 @@
+public class UC4ArraysAndLoops {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                "* * * * * *",
+                "*           *",
+                "*   JAVA    *",
+                "*   UC4     *",
+                "*           *",
+                "* * * * * *"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
